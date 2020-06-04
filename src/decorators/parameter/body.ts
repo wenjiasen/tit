d80e4bcab48a8f8a91fb5065e121c81bc0572029
Jason <wenjsen@gmail.com>
2020-06-04 17:36:58 +0800

    PBody修饰符不支持定义非键值对的参数检查 Fixes #2

--- src/decorators/parameter/body.ts
+++ src/decorators/parameter/body.ts
@@ -5,11 +5,25 @@ import { PARAMETER_BODY_METADATA } from '../constants';
 export type ParameterRouterBodyMetaData = {
   index: number;
   schemaMap: Joi.SchemaMap;
+  isOnlyRoot?: true;
 };
 
 export function PBody(schemaMap: Joi.SchemaMap) {
   return function(target: any, propertyKey: string | symbol, parameterIndex: number): void {
     const data: ParameterRouterBodyMetaData = { index: parameterIndex, schemaMap: schemaMap };
+    Reflect.defineMetadata(PARAMETER_BODY_METADATA, data, target, propertyKey);
+  };
+}
+
+export function PBodyRootArray(schemaMap: Joi.ArraySchema) {
+  return function(target: any, propertyKey: string | symbol, parameterIndex: number): void {
+    const data: ParameterRouterBodyMetaData = {
+      index: parameterIndex,
+      isOnlyRoot: true,
+      schemaMap: {
+        root: schemaMap,
+      },
+    };
     Reflect.defineMetadata(PARAMETER_BODY_METADATA, data, target, propertyKey);
   };
 }
