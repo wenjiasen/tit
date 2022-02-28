a466731cea5666303b17673c2eb2e7142e43d4c3
Jason <wenjsen@gmail.com>
2022-02-28 17:46:52 +0800

    更新用例

---
+++ test/loader/index.spec.ts
@@ -0,0 +1,23 @@
+import assert from 'assert';
+import { Application, ApplicationFactory } from '../../src';
+
+test('LoggerLoader test', async () => {
+  const app = new Application();
+  assert(app.logger.debug);
+});
+
+test('Application controller loader test', async () => {
+  const app = await ApplicationFactory.create();
+  assert(app._controllers.length);
+});
+
+test('Application extend loader test', async () => {
+  const app = await ApplicationFactory.create();
+  assert(app._extends.length);
+});
+
+
+test('Application config loader test', async () => {
+  const app = await ApplicationFactory.create();
+  assert(app.config.port===9999);
+});
